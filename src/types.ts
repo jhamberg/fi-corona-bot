547a6398a2d6e06d4bff2156a185dd041d90501e
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2020-03-14 21:16:27 +0200

    Move case interfaces under types

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,19 @@ export interface Bot {
     send(id: number | string, message: Message);
     readonly id: string;
 }
+
+export interface RawCase extends BaseCase {
+    id: string;
+    date: string;
+}
+
+export interface Case extends BaseCase {
+    id: number;
+    date: number;
+}
+
+export interface BaseCase {
+    infectionSource: "unknown" | number;
+    infectionSourceCountry: string;
+    healthCareDistrict: string;
+}
