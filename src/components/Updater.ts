017eacde37d1a6fd5808841f946be899689a0515
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2020-03-14 21:39:09 +0200

    Add district information to reports
    This commit simplifies the status update logic by using shared methods
    for common taks (like grouping by healthcare district). It also adds
    count(s) per district to reports about new cases and an alternative
    output format for when there is only one district.

--- src/components/Updater.ts
+++ src/components/Updater.ts
@@ -40,6 +40,7 @@ class Updater {
     }
 
     private async updateState(): Promise<void> {
+        this.logger.info("Downloading data...");
         const response: AxiosResponse<ApiResponse> = await axios.get(this.api);
         const confirmed = response?.data?.confirmed;
         const recovered = response?.data?.recovered;
@@ -49,21 +50,20 @@ class Updater {
         }
 
         // Convert cases to a more convenient format
-        const infections: List<InfectionCase> = List(confirmed).map(event => ({
-            ...event,
-            id: Number(event.id),
-            date: Date.parse(event.date)
-        }));
+        const infections: List<Case> = List(confirmed).map(parseCase);
+        const recoveries: List<Case> = List(recovered).map(parseCase);
 
-        const previousId = state.last ?? 0;
+        const previousId = state.last;
         const latest = infections.maxBy(inf => inf.id);
 
         const today = new Date();
         today.setHours(0, 0, 0, 0);
         const yesterday = new Date(today);
         const dayBefore = new Date(today);
+        const fourBefore = new Date(today);
         yesterday.setDate(today.getDate() - 1);
         dayBefore.setDate(today.getDate() - 2);
+        fourBefore.setDate(today.getDate() -4);
 
         const casesToday = infections
             .filter(inf => inf.date >= today.getTime());
@@ -71,21 +71,25 @@ class Updater {
             .filter(inf => inf.date > yesterday.getTime() && inf.date < today.getTime());
         const casesDayBefore = infections
             .filter(inf => inf.date > dayBefore.getTime() && inf.date < yesterday.getTime());
+        const recoveriesToday = recoveries
+            .filter(recovery => recovery.date >= today.getTime());
 
-        const districtsToday = casesToday
-            .groupBy(inf => inf.healthCareDistrict)
-            .map(cases => cases.count())
-            .map((count, district) => `${district}: ${count}`)
+        state.infections = infections;
+        state.today = casesToday.count();
+        state.recoveries = recoveriesToday.count();
+        state.yesterday = casesYesterday.count();
+        state.dayBefore = casesDayBefore.count();
+        state.total = confirmed.length - recovered.length;
+        state.totalRecovered = recovered.length;
+
+        const districtsToday = this.districts(casesToday)
+            .sort()
+            .reverse()
+            .map((count, district) => `${district}: +${count}`)
             .join("\n");
 
-        const districtsRecovered = List(recovered)
-            .groupBy(inf => inf.healthCareDistrict)
-            .map(cases => cases.count());
-
-        const districtsTotal = infections
-            .groupBy(inf => inf.healthCareDistrict)
-            .toMap()
-            .map(cases => cases.count())
+        const districtsRecovered = this.districts(recoveries);
+        const districtsTotal = this.districts(infections)
             .mergeWith((sick, recovered) => sick - recovered, districtsRecovered)
             .filter(cases => cases > 0)
             .sort()
@@ -93,32 +97,72 @@ class Updater {
             .map((count, district) => `${district}: ${count}`)
             .join("\n");
 
-        // Announce new cases
-        if (latest.id - previousId > 0) {
-            state.last = latest.id;
-            state.subscriptions.forEach(this.announce.bind(this));
-        }
-
-        state.today = casesToday.count();
-        state.yesterday = casesYesterday.count();
-        state.dayBefore = casesDayBefore.count();
-        state.recovered = recovered.length;
-        state.total = confirmed.length - recovered.length;
         state.districtsToday = districtsToday;
         state.districtsTotal = districtsTotal;
+
+        // Announce new cases
+        if (latest.id - previousId > 0) {
+            this.logger.info("Sending notifications...");
+            const success = state.subscriptions
+                .map((filter, chat) => this.announce(filter, chat, latest.id))
+                .filter(Boolean);
+
+            if (success) {
+                // Only update when all subsribers are up to date
+                state.last = latest.id;
+                this.logger.info("All notifications sent!");
+            }
+        }
+
         await state.persist();
+        this.logger.info("State updated");
     }
 
-    announce(filter: Filter, chat: string): void {
-        const diff = state.last - filter.last;
-        if (diff >= filter.every) {
-            const [service, id] = chat.split("|");
-            const bot = this.bots.get(service);
-            bot.send(id, { text: `🦠 *${diff}* new ${pluralize("case", diff)}!` });
+    announce(filter: Filter, chat: string, latestId: number): boolean {
+        const infections = state.infections.filter(inf => inf.id > filter.last);
+        const diff = infections.count();
 
-            filter.last = state.last;
-            state.subscriptions.set(chat, filter);
+        if (diff < filter.every) {
+            return true;
         }
+
+        const [service, id] = chat.split("|");
+        const bot = this.bots.get(service);
+        const districts = this.districts(infections)
+            .filter(cases => cases > 0)
+            .sort()
+            .reverse();
+
+        try {
+            if (districts.count() === 1) {
+                const text = `🦠 *${diff}* new ${pluralize("case", diff)} in ` +
+                    `${districts.keySeq().first()}!\n\n` +
+                    `Today *+${state.today}* ${pluralize("case", state.today)}.\n` +
+                    `In total *${state.total}* ${pluralize("case", state.total)}.`;
+                bot.send(id, { text });
+            } else {
+                const text = `🦠 *${diff}* new cases!\n\n` +
+                    "*Healthcare Districts:*\n" +
+                    `${districts.map((diff, dist) => `${dist}: +${diff}`).join("\n")}\n\n` +
+                    `Today *+${state.today}* new ${pluralize("case", state.today)}.\n` +
+                    `Total *${state.total}* ${pluralize("case", state.total)}.`;
+                bot.send(id, { text });
+            }
+            filter.last = latestId;
+            state.subscriptions.set(chat, filter);
+            return true;
+        } catch (error) {
+            // This happens when the service cannot be reached
+            this.logger.error(`Failed sending notification to ${chat}`, error);
+            return false;
+        }
+    }
+
+    districts(cases: List<Case>): Map<string, number> {
+        return cases
+            .groupBy(inf => inf.healthCareDistrict)
+            .toMap()
+            .map(cases => cases.count());
     }
 }
 
