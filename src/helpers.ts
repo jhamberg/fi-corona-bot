5ed4aaf117baca12b6d3f407f19401b48fe2606d
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2020-03-14 21:36:48 +0200

    Make helpers functional, add parseCase

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,11 @@
-export function pluralize(singular: string, count): string {
-    return singular + (count > 1 ? "s" : "");
-}
+import { Case, RawCase } from "./types";
+
+export const pluralize = (singular: string, count): string => (
+    singular + (count > 1 ? "s" : "")
+);
+
+export const parseCase = (item: RawCase): Case => ({
+    ...item,
+    id: Number(item.id),
+    date: Date.parse(item.date)
+});
